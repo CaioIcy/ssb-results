f6a98da4e0b2ee7749f10e714a6791eb4cef8c3f
CaioIcy <caio.nardelli@hotmail.com>
2020-05-25 00:00:31 -0300

    Working PoC

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import htmlToImage from 'html-to-image';
+import downloadjs from 'downloadjs';
+
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const images = importAll(require.context('../assets', false, /\.(png|jpe?g|svg)$/));
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      tournament: {
+        name: "Netplay Coronga #4",
+        date: "2020-May-24",
+      },
       slots: [
         {
           character: "fox",
+          skin: "0",
           player: "caioicy",
         },
         {
           character: "falco",
+          skin: "0",
           player: "caioicy",
         },
         {
           character: "marth",
-          player: "caioicy",
-        },
-        {
-          character: "puff",
+          skin: "0",
           player: "caioicy",
         },
         {
           character: "sheik",
+          skin: "0",
           player: "caioicy",
         },
         {
-          character: "captain",
+          character: "fox",
+          skin: "1",
           player: "caioicy",
         },
         {
-          character: "peach",
+          character: "falco",
+          skin: "1",
           player: "caioicy",
         },
         {
-          character: "pikachu",
+          character: "marth",
+          skin: "1",
+          player: "caioicy",
+        },
+        {
+          character: "sheik",
+          skin: "1",
           player: "caioicy",
         },
       ],
@@ -53,7 +74,7 @@ class App extends React.Component {
     const content = [];
     for(let row = 0; row < ch.length; row++) {
       content.push((
-        <div className="row" key={`slot-row-{row}`}>
+        <div className="row" key={`slot-row-${row}`}>
           {ch[row].map((slot, index) => this.renderSlot(slot, (row*cols)+index))}
         </div>
       ));
@@ -62,10 +83,19 @@ class App extends React.Component {
   }
 
   renderSlot(slot, placementIndex) {
+    const img = `${slot.character}_${slot.skin}.png`;
     return (
-      <div key={`slot-${placementIndex}`} style={{backgroundImage: "url(" + logo + ")"}} className="character-slot">
-        <p>#{placementIndex+1} {slot.character}</p>
-        <p>{slot.player}</p>
+      <div key={`slot-${placementIndex}`} style={{backgroundImage: `url(${images[img]})`}} className="character-slot">
+        <p className="slot-text-top">#{placementIndex+1} {slot.player}</p>
+        <p className="slot-text-bot">{slot.character}</p>
+      </div>
+    );
+  }
+
+  renderTournament() {
+    return (
+      <div>
+      {this.state.tournament.name} {this.state.tournament.date}
       </div>
     );
   }
@@ -73,9 +103,25 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        {this.renderSlots()}
+        <div>
+          <div id="export" className="main-card">
+            {this.renderSlots()}
+            {this.renderTournament()}
+          </div>
+        </div>
+
+        <button onClick={this.downloadExport}>
+          Download
+        </button>
       </div>
     );
+  }
+
+  downloadExport() {
+    htmlToImage.toPng(document.getElementById('export'))
+      .then((dataUrl) => {
+        downloadjs(dataUrl, 'export.png');
+      });
   }
 }
 
