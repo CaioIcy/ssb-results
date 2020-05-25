ebdc89c562fa48cc899c1ad2561c4008c7cf7344
CaioIcy <caio.nardelli@hotmail.com>
2020-05-25 00:31:00 -0300

    add deploy.sh script for github pages

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# npm run build
+sed -i 's/\/build/\#\/build/g' .gitignore
+git add build/
+git commit -m "deploy"
+# git subtree push -f --prefix build origin gh-pages
+git push origin `git subtree split --prefix build master`:gh-pages --force
+sed -i 's/\#\/build/\/build/g' .gitignore
