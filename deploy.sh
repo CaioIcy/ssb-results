8c56296476ba687cda971f965d6bf1d43f2a2d50
CaioIcy <caio.nardelli@hotmail.com>
2020-05-25 00:42:22 -0300

    update homepage

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 npm run build
 sed -i 's/\/build/\#\/build/g' .gitignore
 git add build/
-git commit -m "deploy"
+git commit -m "gh-pages-deploy"
 # git subtree push --prefix build origin gh-pages
 git push origin `git subtree split --prefix build master`:gh-pages --force
 sed -i 's/\#\/build/\/build/g' .gitignore
